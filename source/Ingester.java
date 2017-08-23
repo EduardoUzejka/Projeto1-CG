e517b79c0f2e3bfb8421154f269e6fa07b7036a7
14110462 <14110462@W1846757.portoalegre.pucrsnet.br>
2017-08-23 16:44:09 -0300

    ...

--- source/Ingester.java
+++ source/Ingester.java
@@ -1,7 +1,18 @@
 import java.io.*;
 public class Ingester {
-
+	
 	public static void main(String[] args) {
+		try{
+			File file = new File("CC-DataSet\\Paths_D.txt");
+			System.out.println(file.toString());
+			System.out.println(file.exists());
+			String str = "";
+			
+		}
+		catch(Exception e) {
+			System.out.println("Erro");
+		}
+		
 		// TODO Auto-generated method stub
 
 	}
